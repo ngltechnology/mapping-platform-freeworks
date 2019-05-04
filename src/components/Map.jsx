140c3cf2af8ca6b3bc8c05121d5e1aed96047d9f
masatoshi0929 <kamata@bonx.jp>
2019-05-04 18:56:37 +0200

    change props

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,17 +1,17 @@
 import React from "react"
 import { withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-const InnerMap = withGoogleMap(() => (
+const InnerMap = withGoogleMap(props => (
     <GoogleMap
       defaultZoom={13}
-      defaultCenter={this.props.location}
-      center={this.props.location}
+      defaultCenter={props.location}
+      center={props.location}
     >
-      {this.props.quests.map((quest,index) => (
+      {props.quests.map((quest,index) => (
           <Marker 
             position={quest.location}
             key={index}
-            onClick={this.props.markerClicked(quest)}
+            onClick={props.markerClicked(quest)}
           />
         )
       )}
