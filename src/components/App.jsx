150de11d2180a0e5b4d59bd2a566cf8bc9496551
Takanori Fukuyama <takanori.fukuyama@ngl.technology>
2019-05-17 19:49:30 +0900

    Add routing component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,21 @@
-import React, { Component } from 'react';
-import IconButton from "@material-ui/core/IconButton"
-import Avatar from "@material-ui/core/Avatar"
-import Link from "@material-ui/core/Link"
-import { Link as RouterLink } from "react-router-dom"
+import React from 'react';
+import { Route } from "react-router-dom"
 
 import '../styles/App.scss';
-import MapContainer from '../containers/MapContainer';
-import GrowCard from "./Card"
-
-import UserInfoContainer from '../containers/UserInfoContainer'
-import AuthContainer from '../containers/AuthContainer'
+import PrivateRoute from "./PrivateRoute.jsx"
+import MapContainer from "../containers/MapContainer"
+import AuthContainer from '../containers/AuthContainer.js'
+import AccountContainer from "../containers/AccountContainer"
 
 /* eslint-disable-nextline */
 require('dotenv').config()
 
-class App extends Component {
-  
-  componentDidMount(){
-    //this.props.currentLocation()
-  }
-  render() {
-    return (
-      <div className="App">
-        <div>
-          { this.props.uid ? <UserInfoContainer /> : <AuthContainer /> }
-        </div>
-        <MapContainer />
-        <div className="avatarContain">
-          <Link component={RouterLink} to="/account">
-            <IconButton className="avatar">
-              <Avatar className="avatar">T</Avatar>
-            </IconButton>
-          </Link>
-        </div>
-
-        <GrowCard />
-      </div>
-    );
-  }
-}
-
+const App = () => (
+  <React.Fragment>
+    <Route path='/login' component={AuthContainer} />
+    <Route path="/map" component={MapContainer} />
+    <Route path="/account" component={AccountContainer} />
+  </React.Fragment>
+ )
 
 export default App;
