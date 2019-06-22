dcb2925ca0201a4baca2f5695c01e12abe85ede5
Takanori Fukuyama <takanori.fukuyama@gmail.com>
2019-06-22 17:29:28 +0900

    add new routing file

---
+++ src/components/Route.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react"
+import { Route, Redirect } from "react-router-dom"
+import Auth from "./Auth.jsx"
+import StartStepper from "../containers/StepperContainer.js"
+import AppContainer from "../containers/AppContainer.js"
+
+const Routing = () => (
+  <Fragment>
+    <Route path='/login' component={Auth} />
+    <Route path='/start' component={StartStepper} />
+    <Route exact path="/" component={()=><Redirect to="/login" />} />
+    <Route to="/app" component={AppContainer}/>
+  </Fragment>
+)
+export default Routing;
