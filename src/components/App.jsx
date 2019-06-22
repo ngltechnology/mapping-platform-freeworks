91084477e3c158b4171232ddce16cd8b45c078d7
Takanori Fukuyama <takanori.fukuyama@gmail.com>
2019-06-22 17:28:42 +0900

    add mapbox css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,17 @@
 import React from 'react';
-import { Route, Redirect } from "react-router-dom"
-import '../styles/App.scss';
-import Auth from './Auth.jsx'
+import { Route } from "react-router-dom"
+import '../styles/App.scss'
 import AccountContainer from "../containers/AccountContainer"
 import MapContainer from "../containers/MapContainer"
-import StartStepper from "../containers/StepperContainer.js"
 
 /* eslint-disable-nextline */
 require('dotenv').config()
 
 // eslint-disable-next-line react/display-name
-export default ({
-  bottomNumber,
-  pushmap,
-  pushSchedule,
-  pushAccount,
-}) => {
+export default () => {
   return(
     <React.Fragment>
-      <Route path='/login' component={Auth} />
-      <Route path='/start' component={StartStepper} />
-      <Route exact path="/" component={()=><Redirect to="/login" />} />
-      {bottomNumber === 0 && pushmap}
-      {bottomNumber === 1 && pushSchedule}
-      {bottomNumber === 2 && pushAccount}
-      <Route path="/map" component={MapContainer} />
-      <Route path="/account" component={AccountContainer} />
+      <Route path="/app/map" component={MapContainer} />
+      <Route path="/app/account" component={AccountContainer} />
     </React.Fragment>
 )}
